e8d97c75763e3b5cb00b933ce1648d3513aa4ac5
Mishaeli <meshackb59@gmail.com>
2023-03-20 02:42:13 -0700

    y first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * program printing line with put function
+ * return 0 (succes)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
