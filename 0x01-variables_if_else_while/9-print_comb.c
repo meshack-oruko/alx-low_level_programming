a004716dd3e781f2e604a5ea4a3698565a584a0b
Mishaeli <meshackb59@gmail.com>
2023-03-22 00:26:12 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,14 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		if (1 == 9)
-			putchar(1 + '0');
-		else
-		{	putchar(i + '0');
+		putchar (i + '0');
+		if (i != 9)
+		{
 			putchar(',');
 			putchar(' ');
-
 		}
+
 	}
+	putchar('\n');
 	return (0);
 }
