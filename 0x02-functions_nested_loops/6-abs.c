ba24485b22c32918df13ad4f536aad7faec31094
Mishaeli <meshackb59@gmail.com>
2023-03-23 05:23:52 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ *
+ * @a: parameter
+ *
+ * Return: a
+ */
+
+
+int _abs(int a)
+{
+	if (a < 0)
+		a = -(a);
+	else if (a >= 0)
+		a = a;
+	return (a);
+
+}
