adb884faca554c1d00d57e6859e01d6afea0fdd6
Mishaeli <meshackb59@gmail.com>
2023-03-23 09:24:51 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - Last digit
+ * @i: parameter
+ * Return: k
+ */
+
+
+int print_last_digit(int i)
+{
+	int k;
+
+	k = i % 10;
+	if (i < 0)
+		k = -k;
+	return (k);
+}
